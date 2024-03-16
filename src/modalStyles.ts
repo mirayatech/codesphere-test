b9f428d8354fd477edde9f22fa7154005606b14b
〽️iraya <mirayaabrodi@gmail.com>
2024-03-16 13:02:56 +0100

    style: add styling to modal

---
+++ src/modalStyles.ts
@@ -0,0 +1,76 @@
+import { css } from "lit";
+
+export const modalStyles = css`
+  :host {
+    --modal-background-color: rgba(0, 0, 0, 0.5);
+    --modal-content-background-color: #181621;
+    --modal-content-padding: 20px;
+    --modal-content-width: 300px;
+    --close-button-size: 24px;
+  }
+
+  .modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: var(--modal-background-color);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1000;
+  }
+
+  .modal-content {
+    background-color: var(--modal-content-background-color);
+    padding: var(--modal-content-padding);
+    width: var(--modal-content-width);
+    border-radius: 5px;
+    position: relative;
+    width: 500px;
+  }
+
+  .close {
+    position: absolute;
+    top: 3px;
+    right: 10px;
+    font-size: calc(20 / 16 * 1rem);
+    color: var(--color-white);
+    cursor: pointer;
+  }
+
+  .modal-title {
+    text-align: center;
+    color: var(--color-white);
+    font-size: calc(20 / 16 * 1rem);
+    line-height: 28px;
+    text-align: center;
+    margin-bottom: 50px;
+  }
+  .modal-button {
+    height: 44px;
+    width: 100%;
+    font-size: calc(14 / 16 * 1rem);
+    padding: 6px 12px;
+    color: var(--color-white);
+    background-color: var(--color-primary);
+    border-radius: 3px;
+  }
+
+  input {
+    margin-top: 10px;
+    margin-bottom: 20px;
+    display: block;
+    height: 34px;
+    color: var(--color-light-purple);
+    background-color: transparent;
+    border: 1px solid var(--color-border);
+    border-radius: 3px;
+    padding: 6px 12px;
+  }
+
+  input::placeholder {
+    color: var(--color-light-purple);
+  }
+`;
