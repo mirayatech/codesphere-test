b270f23a73a2b3abfd64b57786929c2f036a67d7
〽️iraya <mirayaabrodi@gmail.com>
2024-03-17 17:58:41 +0100

    add: Add accessibility, make small styling changes, and implement PlusIcon

---
+++ src/components/workspace-table/WorkspaceTable.ts
@@ -0,0 +1,156 @@
+import { html, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { WebSocketDataType, WorkspaceType, ApiService } from "../../util";
+import { WorkspaceTableStyles } from "./WorkspaceTableStyles";
+import "../workspace-modal/WorkspaceModal";
+import "../icons";
+
+@customElement("workspace-table")
+export class WorkspaceTable extends LitElement {
+  @property({ type: Boolean }) isModalOpen = false;
+  @property({ type: Array }) workspaces: WorkspaceType[] = [];
+  @property({ type: String }) newWorkspaceName = "";
+  @property({ type: Number }) dropdownVisibleFor: number | null = null;
+  @property({ type: Number }) teamId = 1;
+
+  static styles = [WorkspaceTableStyles];
+
+  private apiService: ApiService;
+
+  constructor() {
+    super();
+    this.apiService = new ApiService(this.teamId);
+  }
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    this.initializeServiceAndData();
+  }
+
+  initializeServiceAndData() {
+    this.fetchWorkspaces();
+    this.setupWebSocket();
+  }
+
+  async fetchWorkspaces() {
+    try {
+      const workspaces = await this.apiService.listWorkspaces();
+      this.workspaces = workspaces;
+    } catch (error) {
+      console.error("Error fetching workspaces:", error);
+    }
+  }
+
+  setupWebSocket() {
+    this.apiService.connectToWebSocket((data: WebSocketDataType) => {
+      if (data.deleted) {
+        this.workspaces = this.workspaces.filter(
+          (workspace) => workspace.id !== data.id
+        );
+      } else {
+        const index = this.workspaces.findIndex(
+          (workspace) => workspace.id === data.id
+        );
+        if (index >= 0) {
+          this.workspaces[index] = { ...this.workspaces[index], ...data };
+        } else {
+          this.workspaces.push({
+            id: data.id,
+            name: data.name || "Unnamed Workspace",
+          });
+        }
+      }
+      this.requestUpdate();
+    });
+  }
+
+  render() {
+    return html`
+      <div class="workspace" role="region">
+        <button
+          class="workspace-button"
+          @click="${() => (this.isModalOpen = true)}"
+          aria-label="Add new workspace"
+        >
+          <plus-icon></plus-icon> New workspace
+        </button>
+        <workspace-modal
+          ?isOpen="${this.isModalOpen}"
+          @close-modal="${() => (this.isModalOpen = false)}"
+          @create-workspace="${this.handleCreateWorkspace}"
+          aria-label="Create a new workspace"
+        ></workspace-modal>
+        <table class="workspace-table">
+          <tr class="table-row">
+            <th class="table-header">Name</th>
+            <th class="table-header">Actions</th>
+          </tr>
+          <tbody>
+            ${this.workspaces.map(
+              (workspace) => html`
+                <tr class="table-row">
+                  <td class="table-data">${workspace.name}</td>
+                  <td class="table-data">
+                    <button
+                      class="dropdown-button"
+                      @click="${() => this.toggleDropdown(workspace.id)}"
+                      aria-label="Options for ${workspace.name}"
+                      aria-haspopup="true"
+                      aria-expanded="${this.dropdownVisibleFor === workspace.id
+                        ? "true"
+                        : "false"}"
+                    >
+                      <ellipsis-icon aria-hidden="true"></ellipsis-icon>
+                    </button>
+                    ${this.dropdownVisibleFor === workspace.id
+                      ? html`
+                          <div class="dropdown-content" role="menu">
+                            <button
+                              class="dropdown"
+                              @click="${() =>
+                                this.deleteWorkspace(workspace.id)}"
+                              role="menuitem"
+                            >
+                              <trash-icon aria-hidden="true"></trash-icon>
+                              Delete
+                            </button>
+                          </div>
+                        `
+                      : null}
+                  </td>
+                </tr>
+              `
+            )}
+          </tbody>
+        </table>
+      </div>
+    `;
+  }
+
+  handleCreateWorkspace(event: CustomEvent) {
+    this.createWorkspace(event.detail);
+  }
+
+  async createWorkspace(workspaceName: string) {
+    if (workspaceName.trim() !== "") {
+      await this.apiService.createWorkspace(workspaceName);
+      this.fetchWorkspaces();
+    }
+    this.closeModal();
+  }
+
+  async deleteWorkspace(workspaceId: number) {
+    await this.apiService.deleteWorkspace(workspaceId);
+    this.dropdownVisibleFor = null;
+    this.fetchWorkspaces();
+  }
+
+  toggleDropdown(workspaceId: number) {
+    this.dropdownVisibleFor =
+      this.dropdownVisibleFor === workspaceId ? null : workspaceId;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+}
---
+++ src/components/workspace-table/WorkspaceTableStyles.ts
@@ -0,0 +1,112 @@
+import { css } from "lit";
+
+export const WorkspaceTableStyles = css`
+  :host {
+    width: 100%;
+    max-width: 520px;
+  }
+
+  .workspace {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    box-sizing: border-box;
+  }
+
+  .workspace-table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 1em;
+  }
+
+  .table-header,
+  .table-data {
+    text-align: left;
+    border-bottom: 1px solid var(--color-border);
+  }
+
+  .table-header {
+    width: 450px;
+    height: 28px;
+    padding: 5px 16px 5px 8px;
+    gap: 10px;
+
+    font-size: calc(12 / 16 * 1rem);
+    font-weight: 400;
+    color: var(--color-medium-gray);
+    background-color: var(--color-navy-purple);
+  }
+  .table-data {
+    height: 60px;
+    font-size: calc(14 / 16 * 1rem);
+    font-weight: 600;
+    text-align: left;
+    color: var(--color-light-purple);
+    padding: 12px 16px 12px 8px;
+  }
+
+  .table-header:first-child,
+  .table-data:first-child {
+    width: 450px;
+  }
+
+  .table-header:last-child,
+  .table-data:last-child {
+    width: 70px;
+    position: relative;
+    text-align: right;
+  }
+
+  .workspace-button {
+    gap: 8px;
+    display: flex;
+    align-items: center;
+    margin-left: auto;
+    margin-bottom: 20px;
+    background-color: var(--color-primary);
+    width: 150px;
+    height: 34px;
+    padding: 6px 12px;
+    border-radius: 3px;
+    font-size: calc(14 / 16 * 1rem);
+    font-weight: 400;
+    line-height: 22px;
+    border: none;
+    color: var(--color-white);
+    transition: background-color 0.2s ease-in-out;
+  }
+
+  .workspace-button:hover {
+    background-color: var(--color-primary-hover);
+  }
+  .dropdown-button {
+    font-size: 1em;
+    padding: 0.5em;
+    color: var(--color-light-purple);
+    background-color: transparent;
+    border: none;
+    border-radius: 50%;
+    cursor: pointer;
+    margin-left: auto;
+  }
+
+  .dropdown {
+    font-weight: 300;
+    position: absolute;
+    background-color: var(--color-navy-purple);
+    box-shadow: 0px 5px 10px 0px #0000001a;
+    padding: 8px 12px;
+    z-index: 1;
+    border: 1px solid var(--color-border);
+    border-radius: 3px;
+    width: 226px;
+    height: 50px;
+    top: 60px;
+    right: 0;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    color: var(--color-light-gray);
+    font-size: calc(14 / 16 * 1rem);
+  }
+`;
